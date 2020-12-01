fe1c9d96b30d641372ad5774b2ed783743027a04
Marc Boquet <marc.boquet@gmail.com>
2020-12-01 19:53:33 +0100

    Day 1 part 2

--- Sources/AdventOfCode2020/ExpenseReport.swift
+++ Sources/AdventOfCode2020/ExpenseReport.swift
@@ -18,4 +18,19 @@ struct ExpenseReport {
         }
         return (0, 0)
     }
+    
+    func findSum(sum: Int) -> (Int, Int, Int) {
+        for (i, first) in expenses.enumerated() {
+            for j in i..<expenses.count {
+                for k in j..<expenses.count {
+                    let second = expenses[j]
+                    let third = expenses[k]
+                    if first + second + third == sum {
+                        return (first, second, third)
+                    }
+                }
+            }
+        }
+        return (0, 0, 0)
+    }
 }
--- Tests/AdventOfCode2020Tests/Day1Tests.swift
+++ Tests/AdventOfCode2020Tests/Day1Tests.swift
@@ -2,9 +2,15 @@ import XCTest
 @testable import AdventOfCode2020
 
 final class Day1Tests: XCTestCase {
-    func testExample() {
+    func testPart1() {
         let report = ExpenseReport(expenses: AdventOfCode2020(name: "1-input").lines())
-        let numbers = report.findSum(sum: 2020)
+        let numbers: (Int, Int) = report.findSum(sum: 2020)
         XCTAssertEqual(numbers.0 * numbers.1, 878724)
+    }
+    
+    func testPart2() {
+        let report = ExpenseReport(expenses: AdventOfCode2020(name: "1-input").lines())
+        let numbers: (Int, Int, Int) = report.findSum(sum: 2020)
+        XCTAssertEqual(numbers.0 * numbers.1 * numbers.2, 201251610)
     }
 }
