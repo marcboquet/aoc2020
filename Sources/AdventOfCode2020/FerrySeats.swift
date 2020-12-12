78b04e84c8986fcda778a4fc02662a894ca04b39
Marc Boquet <marc.boquet@gmail.com>
2020-12-12 09:26:07 +0100

    Day 11 part 2

--- Sources/AdventOfCode2020/FerrySeats.swift
+++ Sources/AdventOfCode2020/FerrySeats.swift
@@ -11,28 +11,30 @@ struct FerrySeats {
         self.seats = seats
     }
     
-    func runModel() -> [[Seat]] {
+    func runModel(extended: Bool = false) -> [[Seat]] {
         var prevSeats = self.seats
         var newSeats = self.seats
         repeat {
             prevSeats = newSeats
-            newSeats = runRound(prevSeats)
+            newSeats = runRound(prevSeats, extended: extended)
         } while newSeats != prevSeats
         
         return newSeats
     }
     
-    func runRound(_ seats: [[Seat]]) -> [[Seat]] {
+    func runRound(_ seats: [[Seat]], extended: Bool) -> [[Seat]] {
         var newSeats = seats
+        let maxOccupied = (extended ? 5 : 4)
         for row in 0..<seats.count {
             let rowSeats = seats[row]
             for col in 0..<rowSeats.count {
                 let seat = rowSeats[col]
                 if seat == .floor { continue }
-                let adjacent = adjacentSeats(to: (row, col), in: seats)
-                if seat == .empty && adjacent.filter({ $0 == .occupied }).count == 0 {
+                let max = (seat == .empty ? 1 : maxOccupied)
+                let adjacent = adjacentSeats(to: (row, col), in: seats, max: max, extended: extended)
+                if seat == .empty && adjacent == 0 {
                     newSeats[row][col] = .occupied
-                } else if seat == .occupied && adjacent.filter({ $0 == .occupied }).count >= 4 {
+                } else if seat == .occupied && adjacent >= maxOccupied {
                     newSeats[row][col] = .empty
                 }
             }
@@ -40,18 +42,44 @@ struct FerrySeats {
         return newSeats
     }
     
-    func adjacentSeats(to position: (Int, Int), in seats: [[Seat]]) -> [Seat] {
-        var adjacent = [Seat]()
-        for rowOffset in -1...1 {
-            for colOffset in -1...1 {
+    func adjacentSeats(to position: (Int, Int), in seats: [[Seat]], max: Int, extended: Bool) -> Int {
+        var occupied = 0
+        let limits = (seats.count-1, seats[0].count-1)
+        for rowOffset in [-1,0,1] {
+            if position.0 == 0 && rowOffset == -1 { continue }
+            if position.0 == limits.0 && rowOffset == 1 { return occupied }
+            for colOffset in [-1,0,1] {
                 if rowOffset == 0 && colOffset == 0 { continue }
-                if !seats.indices.contains(position.0 + rowOffset) { continue }
-                let row = seats[position.0 + rowOffset]
-                if !row.indices.contains(position.1 + colOffset) { continue }
-                adjacent.append(row[position.1 + colOffset])
+                if position.1 == 0 && colOffset == -1 { continue }
+                if position.1 == limits.1 && colOffset == 1 { continue }
+
+                let seat = seats[position.0 + rowOffset][position.1 + colOffset]
+                if seat == .occupied {
+                    occupied += 1
+                } else if extended && seat == .floor {
+                    var round = 2
+                    var otherOffset: (Int, Int)
+                    while true {
+                        otherOffset = (position.0 + rowOffset * round, position.1 + colOffset * round)
+                        if otherOffset.0 < 0 || otherOffset.1 < 0 || otherOffset.0 > limits.0 || otherOffset.1 > limits.1 {
+                            break
+                        }
+                        let otherSeat = seats[otherOffset.0][otherOffset.1]
+                        if otherSeat == .occupied {
+                            occupied += 1
+                            break
+                        }
+                        if otherSeat == .empty {
+                            break
+                        }
+                        round += 1
+                    }
+                    
+                }
+                if occupied >= max { return occupied }
             }
         }
-        return adjacent
+        return occupied
     }
     
     func occupied(seats: [[Seat]]) -> Int {
--- Tests/AdventOfCode2020Tests/Day11Tests.swift
+++ Tests/AdventOfCode2020Tests/Day11Tests.swift
@@ -17,5 +17,19 @@ final class Day11Tests: XCTestCase {
 
         XCTAssertEqual(ferry.occupied(seats: ferry.runModel()), 2310)
     }
+    
+    func testPart2Sample() {
+        let input: [String] = Input(name: "11-sample").lines()
+        let ferry = FerrySeats(input)
+
+        XCTAssertEqual(ferry.occupied(seats: ferry.runModel(extended: true)), 26)
+    }
+    
+    func testPart2() {
+        let input: [String] = Input(name: "11-input").lines()
+        let ferry = FerrySeats(input)
+
+        XCTAssertEqual(ferry.occupied(seats: ferry.runModel(extended: true)), 2074)
+    }
 }
 
